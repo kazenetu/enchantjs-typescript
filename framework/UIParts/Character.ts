b4123d82b4bfa7410541116becb5e5c3f989cea4
kazenetu <rakudafactory@gmail.com>
2016-04-15 23:09:13 +0900

    アニメーションの速度設定と停止・再開機能の追加

--- framework/UIParts/Character.ts
+++ framework/UIParts/Character.ts
@@ -20,10 +20,17 @@ namespace Rf.ETS.FrameWork {
     export class Character extends Rf.ETS.FrameWork.Sprite implements Rf.ETS.FrameWork.IRunnable 
     {
         public charaIndex: number = 0;
+        
+        public set Dir(value: Direction) {
+            this.dir = value;
+            this.SetFrame();
+        }
+        public maxWaitCount:number = 1;
 
-        public waitCount: number = 0;
-        public dir: Direction = Direction.Down;
-        public anime: number = 0;
+        private dir: Direction = Direction.Down;
+        private waitCount: number = 0;
+        private anime: number = 0;
+        private isRunAnime:boolean = true;
 
         /**
          * コンストラクタ
@@ -46,26 +53,83 @@ namespace Rf.ETS.FrameWork {
          */
         public Run(): boolean 
         {
-            this.SetAnime();
-
-            //フレーム処理
-            this.frame = this.charaIndex * 2 + this.dir * 26;
-            if (this.anime >= 2) {
-                this.frame += 1;
+            if(this.SetAnime()){
+                //フレーム処理
+                this.SetFrame();
             }
-
             return true;
+        }
+        
+        /**
+         * アニメ処理の休止
+         * @method
+         * @name UIParts.Character#SuspendAnime
+         */
+        public SuspendAnime():void{
+            this.isRunAnime = false;
+        }
+
+        /**
+         * アニメ処理の再開
+         * @method
+         * @name UIParts.Character#ResumeAnime
+         */
+        public ResumeAnime():void{
+            this.isRunAnime = true;
+        }
+        
+        /**
+         * アニメ処理の状態取得
+         * @method
+         * @name UIParts.Character#IsRunAnime
+         * @return {boolean} 実行中はtrue 停止中はfalse
+         */
+        public IsRunAnime():boolean{
+            return this.isRunAnime;
         }
 
         /**
          * アニメ実行
          * @method
          * @name UIParts.Character#SetAnime
+         * @return {boolean} フレーム更新実施の可否
          */
-        public SetAnime():void 
+        private SetAnime():boolean 
         {
-            if (++this.anime >= 4) {
-                this.anime = 0;
+            if(this.isRunAnime === false){
+                return false;
+            }
+            let isUpdateFrame:boolean = true;
+            
+            if(this.maxWaitCount <= 0){
+                return false;
+            }else{
+                if(this.waitCount < this.maxWaitCount){
+                    isUpdateFrame=false;
+                }
+                this.waitCount++;
+            }
+            
+            if(isUpdateFrame){
+                this.anime++;
+                if (this.anime >= 4) {
+                    this.anime = 0;
+                }
+                this.waitCount = 0;
+            }
+            return isUpdateFrame;
+        }
+        
+        /**
+         * フレームの更新
+         * @method
+         * @name UIParts.Character#SetFrame
+         * @return {boolean} フレーム更新実施の可否
+         */
+        private SetFrame():void{
+            this.frame = this.charaIndex * 2 + this.dir * 26;
+            if (this.anime >= 2) {
+                this.frame += 1;
             }
         }
     }
