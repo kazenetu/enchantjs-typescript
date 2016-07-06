45dd9b76a65413aac09834f7e2b7c0bc9b5d8afc
kazenetu <rakudafactory@gmail.com>
2016-07-06 22:50:31 +0900

    サンプルフォルダ内にnode_modulesが存在している場合でビルドエラーが出ていた不具合を修正

--- starter/gulpfile.js
+++ starter/gulpfile.js
@@ -3,7 +3,7 @@ var typescript = require('gulp-typescript');
 
 //サンプルのビルド
 gulp.task('buildSimple', function () {
-  return gulp.src(['./simple/**/*.ts'])
+  return gulp.src(['./simple/**/*.ts','!./**/node_modules/**/*.ts'])
     .pipe(typescript('./simple/tsconfig.json')).js
     .pipe(gulp.dest('./simple'));
 });
@@ -16,22 +16,22 @@ gulp.task('buildUseFramework',
   ], function () {
   });
 gulp.task('buildUseFrameworkImageSpriteSample', function () {
-  return gulp.src(['./use-framework/ImageSpriteSample/**/*.ts'])
+  return gulp.src(['./use-framework/ImageSpriteSample/**/*.ts','!./**/node_modules/**/*.ts'])
     .pipe(typescript('./use-framework/ImageSpriteSample/tsconfig.json')).js
     .pipe(gulp.dest('./use-framework/ImageSpriteSample'));
 });
 gulp.task('buildUseFrameworkLoginSample', function () {
-  return gulp.src(['./use-framework/LoginSample/**/*.ts'])
+  return gulp.src(['./use-framework/LoginSample/**/*.ts','!./**/node_modules/**/*.ts'])
     .pipe(typescript('./use-framework/LoginSample/tsconfig.json')).js
     .pipe(gulp.dest('./use-framework/LoginSample'));
 });
 gulp.task('buildUseFramework2DRPGMapSample', function () {
-  return gulp.src(['./use-framework/2DRPGMapSample/**/*.ts'])
+  return gulp.src(['./use-framework/2DRPGMapSample/**/*.ts','!./**/node_modules/**/*.ts'])
     .pipe(typescript('./use-framework/2DRPGMapSample/tsconfig.json')).js
     .pipe(gulp.dest('./use-framework/2DRPGMapSample'));
 });
 gulp.task('buildUseFrameworkTemplate', function () {
-  return gulp.src(['./use-framework/template/**/*.ts'])
+  return gulp.src(['./use-framework/template/**/*.ts','!./**/node_modules/**/*.ts'])
     .pipe(typescript('./use-framework/template/tsconfig.json')).js
     .pipe(gulp.dest('./use-framework/template'));
 });
